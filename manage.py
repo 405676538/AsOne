a6958b77148188bf3b1679930e632dae43fb8cd3
唐浩 <15928265276@163.com>
2019-04-10 19:30:54 +0800

    veersion update

--- manage.py
+++ manage.py
@@ -71,7 +71,7 @@ def Is_child_processing():
 
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled4.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
